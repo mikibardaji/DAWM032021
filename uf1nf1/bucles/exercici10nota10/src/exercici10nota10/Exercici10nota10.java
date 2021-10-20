8ce3e5ba910e111f4730fa6afb4195f845e0e86c
Tu Nombre <ma@example.com>
2021-10-20 15:19:02 +0200

    20102021b

--- uf1nf1/bucles/exercici10nota10/src/exercici10nota10/Exercici10nota10.java
+++ uf1nf1/bucles/exercici10nota10/src/exercici10nota10/Exercici10nota10.java
@@ -17,18 +17,22 @@ public class Exercici10nota10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int nota,contanotas=0;
+        int nota,contanotas=0,acumNotas=0;
          Scanner teclado = new Scanner(System.in);
          boolean hubo10=false;
          
          do{
              System.out.print("pon nota:");
             nota=teclado.nextInt();
-            if (nota!=-1)
+            if (nota!=-1) //me cuenta las veces que pongo notas, sin contar el -1
             {
-             contanotas++;    
-            }
-            
+             contanotas++;  //se cuantas notas he puesto 
+             acumNotas = acumNotas+nota; //voy sumnado notas a cada vuelta         
+//              if (nota==10)
+//            {
+//                hubo10=true;
+//            }            
+            }           
             if (nota==10)
             {
                 hubo10=true;
@@ -38,15 +42,13 @@ public class Exercici10nota10 {
          
          
          if (hubo10==true)
-         {
-             System.out.println("pusiste un 10");
-         }
+         {System.out.println("pusiste un 10");}
          else
-         {
-             System.out.println("no hay 10");
-         }
+         {System.out.println("no hay 10");}
          
-         System.out.println("has puesto " + contanotas + "notas ");
+         System.out.println("has puesto " + contanotas + " notas ");
+         double media=(double) acumNotas/contanotas;
+         System.out.println("La media es" + media);
     }
     
 }
