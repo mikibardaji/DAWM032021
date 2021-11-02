8a6e718d414acaba3428485d5d7b2266f052a2bf
Tu Nombre <ma@example.com>
2021-11-02 15:04:31 +0100

    20211102

--- uf2nf1/CalculadoraSumaResta/src/calculadorasumaresta/CalculadoraSumaResta.java
+++ uf2nf1/CalculadoraSumaResta/src/calculadorasumaresta/CalculadoraSumaResta.java
@@ -18,21 +18,129 @@ public class CalculadoraSumaResta {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int valor1,valor2;
-        int resultado=0; //operacion 
+        double resultado=0; //operacion 
         int opcion;
-        System.out.println("Pon el primer valor");
-        valor1 = sc.nextInt();
-        System.out.println("Pon el segundo valor");
-        valor2 = sc.nextInt();
-        System.out.println("1.- Calcular suma");
-        System.out.println("2.- Calcular resta");
-        System.out.print("Pon opcion");
-        opcion = sc.nextInt();
         
-        
+        mostrarFraseValor(1);
+        valor1=introducirPositivo(sc);
+        mostrarFraseValor(2);
+        valor2 = introducirPositivo(sc);
+        mostrarMenu();
+        opcion = validarOpcion(sc);
+        if (opcion==1)
+        {
+            resultado = calcularSuma(valor1,valor2);
+        }
+        else if(opcion==2)
+        {
+            resultado = calcularResta(valor1, valor2);
+        }
+        else if(opcion==3)
+        {
+            resultado = calcularMulti(valor1, valor2);
+        } 
+        else if(opcion==4)
+        {
+            resultado = calcularDivision(valor1, valor2);
+        } 
         
         System.out.println("El resultado de la operación elegida es " + resultado);
         
     }
+
+    public static int calcularSuma(int valor1, int valor2) {
+        int calc = valor1+valor2;
+        return calc;
+    }
+
+    private static int calcularResta(int valor1, int valor2) {
+        int calc = valor1-valor2;
+        return calc;
+    }
+
+    public static int calcularMulti(int valor1, int valor2) {
+          int calc = valor1*valor2;
+        return calc;
+    }
+
+    public static double calcularDivision(int valor1, int valor2) {
+         double calc = (double) valor1/valor2;
+        return calc;
+    }
+
+    /**
+     * te muestra el menu de la calculadora
+     */
+    public static void mostrarMenu() {
+        System.out.println("1.- Calcular suma");
+        System.out.println("2.- Calcular resta");
+        System.out.println("3.- Calcular multiplicación");
+        System.out.println("4.- Calcular división"); 
+    }
+
+    /**
+     * pedirte la opción de la calculadora y solo te aceptara
+     * del 1 al 4, sino volvera a pedir el 
+     * @return 
+     */
+    /*private static int validarOpcion() {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do{  
+            System.out.print("Pon opcion menu(1-4): ");
+            opcion = sc.nextInt();
+            if(opcion <1 || opcion>4)
+                {
+                    System.out.println("opción incorrecta");
+                    System.out.println("*****************");
+                    mostrarMenu();
+                }
+        }while(opcion <1 || opcion>4);
+        //Salir opcion >=1 && opcion<=4
+        return opcion;
+    }*/
+    private static int validarOpcion(Scanner sc) {
+        int opcion;
+        do{  
+            System.out.print("Pon opcion menu(1-4): ");
+            opcion = sc.nextInt();
+            if(opcion <1 || opcion>4)
+                {
+                    System.out.println("opción incorrecta");
+                    System.out.println("*****************");
+                    mostrarMenu();
+                }
+        }while(opcion <1 || opcion>4); //Salir opcion >=1 && opcion<=4
+        return opcion;
+    }
+
+    /**
+     * funcion introducirPositivo
+     * Devuelve un entero solo cuando es mayor que 0
+     * sino vuelve a pedir que introduzcan datos
+     * 
+     */
+    public static int introducirPositivo(Scanner sc) {
+            int valor;
+            do
+            {
+                System.out.println("Introduce un valor positivo");
+                valor = sc.nextInt();
+                if(valor<=0)
+                {
+                    System.out.println("Burro el numero " + valor + " no es positivo ");
+                }
+            }while(valor<=0); //si es negativo o 0 volvere apedir
+            
+            
+            
+            return valor;
+    }
+
+    private static void mostrarFraseValor(int i) {
+        System.out.println("Valor "+ i);
+    }
+    
+    
     
 }
